a76a61c75ca14ddebf20902115bbad6475b70bb9
Yohans Samuel <yohanssamuel2014@gmail.com>
2022-09-17 16:35:14 +0300

    add token preprocessing function

--- scripts/preprocessing.py
+++ scripts/preprocessing.py
@@ -1,5 +1,19 @@
+from html import entities
 import pandas as pd
 
 class Preporcess:
     def __init__(self) -> None:
         pass
+
+    def preprocess_tokens(self,tokens):
+        entities = {}
+        for token in tokens:
+
+            if token['entityLabel'] in entities.keys():
+                entities[token['entityLabel']] = entities[token['entityLabel']] + ','+ token['text']
+            else:
+                entities[token['entityLabel']] = token['text']
+        label = ""
+        for k, v in entities.items():
+            label += k+':'+v+"\n"
+        return label
